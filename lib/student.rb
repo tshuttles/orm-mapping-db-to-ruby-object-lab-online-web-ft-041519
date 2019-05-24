bfa4954fd3dbb42ea8a96881746ca3f82bbc72ad
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 03:57:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,8 @@ class Student
       FROM Students
     SQL
  
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 
   def self.find_by_name(name)
