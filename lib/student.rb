bfeb5b30a1c65884e4ab28ce654c8acf58bd624d
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 03:55:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,4 +61,21 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9 
+  end 
+  
+  def self.students_below_12th_grade 
+  end 
+  
+  def self.first_X_students_in_grade_10 
+  end 
+  
+  def self.first_student_in_grade_10 
+  end 
+  
+  def self.all_students_in_grade_X 
+  end 
 end
+
+
