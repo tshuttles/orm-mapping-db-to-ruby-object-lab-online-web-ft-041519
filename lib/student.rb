262871624f6b48731c68442fc65ee28efe23b6a8
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 03:48:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,10 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM Students 
+      WHERE name = ? 
   end
   
   def save
