c5686835c62421283b504bea9d71cbe1b58a9960
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-24 03:59:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
       WHERE grade = 9  
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, name, grade).map do |row|
       self.new_from_db(row)
     end
   end 
